4bd499e641ab3c781e16b915917f867cc4efa31a
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-12 10:43:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,11 +5,15 @@ def turn_count(board)
     if plays == "X" || plays == "O"
       counter += 1
     end
-
   end
-return counter
+
+  return counter
 end
 
 def current_player(board)
-
+  if turn_count.counter % 2 == 0
+    current_player = "X"
+  else
+    current_player = "O"
+  end
 end
